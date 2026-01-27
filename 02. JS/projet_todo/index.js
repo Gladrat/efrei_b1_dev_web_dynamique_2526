92bacf79181835dbc28dcddae41a2e633229d5ad
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-27 12:47:12 +0100

    update

--- 02. JS/projet_todo/index.js
+++ 02. JS/projet_todo/index.js
@@ -10,7 +10,7 @@ const tasks = ["Apprendre HTML", "Apprendre le CSS", "Apprendre le JS"];
 const h1 = document.querySelector("h1");
 h1.textContent = "MA TODO LIST:";
 
-for (const t of tasks) {
+function addTask(t) {
   if (typeof t === "string" && t) {
     const li = document.createElement("li");
     const remove = document.createElement("button");
@@ -18,7 +18,27 @@ for (const t of tasks) {
     li.textContent = t;
     remove.textContent = "REMOVE";
 
+    remove.addEventListener("click", (event) =>
+      event.target.parentNode.remove(),
+    );
+
     li.append(remove);
     list.append(li);
   }
+  input.value = "";
+  input.focus();
 }
+
+for (const t of tasks) {
+  addTask(t);
+}
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask(input.value);
+  }
+});
+
+add.addEventListener("click", (e) => {
+  addTask(input.value);
+});
