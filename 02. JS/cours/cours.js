8a1962b1bfdedc916e5f737b291daca86b91c0cc
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-28 08:25:08 +0100

    update

--- 02. JS/cours/cours.js
+++ 02. JS/cours/cours.js
@@ -1,42 +1,20 @@
-// imports
-// defer
-// use strict
+// function hello() {
+//     console.log("Hello!");
+// }
 
-// let age = prompt("Quel est votre age ?")
-// age = Number(age)
+// setTimeout(hello, 2500);
 
-// if (! isNaN(age)) {
-//   console.log("Votre age est:", age, "ans");
-//   console.log(`votre age est ${age} ans`);
+// // ======== EQUIVALENT (ou presque) ========
+
+// setTimeout(() => {
+//   console.log("Hello!");
+// }, 2500);
+
+// function detectClick() {
+//   console.log("click detected!!!");
 
 // }
 
-// let r = confirm("Vous êtes sûr ?")
-// console.log(r);
-
-// let arr = [1, 2, 3];
-// console.log(arr);
-
-// arr = [];
-// console.log(arr);
-
-const arr = [];
-
-arr.push(99);
-arr.push(98);
-arr.push(97);
-arr.push(96);
-arr.push(95);
-
-console.log(arr);
-console.log(arr.length);
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-
-arr.forEach((e) => {
-  console.log(e);
-  console.log(e ** 2);
-  console.log("-");
-});
+document
+  .querySelectorAll("button")
+  .forEach((e) => e.addEventListener("click", (event) => console.log(event)));
