65557a0a1d064eabdeeea095872d62d32bec1d95
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-02-11 09:08:15 +0100

    update

---
+++ 03. PHP/demo/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Ceci est une page de test avec des balises PHP</title>
+  <metacharset="utf-8" />
+</head>
+
+<body>
+  Cette page contient du code HTML avec des balises PHP.<br />
+  <?php
+  /* Code PHP */
+  ?>
+</body>
+
+</html>
